d33a03c385998936fa913e7e0fcd424992a52117
rmishra00 <riya@nostacklabs.com>
2026-07-03 16:24:45 +0530

    added lua script

--- redislimiters/RedisFixedWindowRateLimiter.js
+++ redislimiters/RedisFixedWindowRateLimiter.js
@@ -1,19 +1,22 @@
 const {redisClient} = require('../redisClient');
+const fs = require('fs');
+
 
 class RedisFixedWindowRateLimiter{
   constructor(limit, windowSec){
-    this.limit = limit,
-    this.windowMs = windowMs
+    this.limit = limit;
+    this.windowSec = windowSec;
+    this.fixedWindowLuaScript = fs.readFileSync('./lua/fixedWindow.lua', 'utf-8')
   }
   async allowRequest(ip){
     const key = `rate-limit:${ip}`;
-    const count = await redisClient.incr(key);
-    if(count === 1){
-      await redisClient.expire(
-        key,
-        this.windowMs
-      )
-    }
+    const count = await redisClient.eval(
+      this.fixedWindowLuaScript,
+      {
+        keys:[key],
+        arguments:[this.windowSec]
+      }
+    )
     if(count>this.limit){
       return{
         allowed:false,
